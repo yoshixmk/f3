a047150b1c1ab60cd75a34097e9e4073d4ca1e36
yoshixmk <pirori67@gmail.com>
2020-09-05 11:40:39 +0900

    mjs. fix express warinig message at frontend

---
+++ src/main.mjs
@@ -0,0 +1,33 @@
+import {createApp, h} from "vue";
+import store from "./store";
+
+import VoteApp from "./VoteApp.vue";
+import FacilitatorApp from "./FacilitatorApp.vue";
+import "popper.js";
+import "bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const NotFoundComponent = {template: "<p>Page not found</p>"};
+
+const routes = {
+	"/": VoteApp,
+	"/fff": FacilitatorApp,
+};
+
+export const router = {
+	data: () => ({
+		currentRoute: window.location.pathname,
+	}),
+
+	computed: {
+		CurrentComponent() {
+			return routes[this.currentRoute] || NotFoundComponent;
+		},
+	},
+
+	render() {
+		return h(this.CurrentComponent);
+	},
+};
+
+createApp(router).use(store).mount("#app");
